8bb50b18c3b5cb3c11a4ac6f10b5e99c0d46132f
camidilaudo <camiladilaudo@icloud.com>
2024-09-03 21:28:50 -0300

    arregle los errores de sintaxis

--- utils/users_utils.py
+++ utils/users_utils.py
@@ -6,21 +6,22 @@ def registrar_usuario(nombre_usuario, contrasenia_usuario, contrasenia_usuario2)
     lista_contrasenas = ud.contrasenas
     usuario_registrado = True
     # Verifica si el nombre de usuario ya existe
-    if nombre_usuario in usuarios:
+    if nombre_usuario in ud.usuarios:
         usuario_registrado = False
     # Verifica si las contraseñas son iguales
-    elif contrasenia_usuario != lista_contrasenas:
+    elif contrasenia_usuario != contrasenia_usuario2:
         usuario_registrado = False
     else:
         nombre_usuario.append(lista_usuarios)
-        contrasena.append(lista_contrasenas)
+        contrasenia_usuario.append(lista_contrasenas)
     return usuario_registrado
 
 
 # TODO: hay que cambiar la variable 'a' a una mas descriptiva y los inputs deben ir solo en el main, separado de la
 #  logica de las funciones
 def login_usuario(a):
-    """Funcion para loguear el usuario. La corriente función busca poder disernir si la persona interesada busca ingresar
+    """Funcion para loguear el usuario. La corriente función busca poder disernir si la persona interesada busca
+    ingresar
     al sistema como usuario o como administrador.
     :param a: Str, tipo de usuario. 1 es usuario y 2 es administrador.
     :return:Str, tipo_de_usuario.
