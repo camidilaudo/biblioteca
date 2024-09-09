6c1344b2bdc9213f5a85098abbe95bce828550de
Daniela Melian <meliandaani@gmail.com>
2024-09-08 23:55:51 -0300

    agregar funcion de impresion, main de bibliotecario y sacar los inputs de editar_libro y pasarlos al main

--- data_store/users_data.py
+++ data_store/users_data.py
@@ -9,5 +9,9 @@ contrasenia_general = "ADMIN123"
 # MATRIZ DE HISTORIAL
 
 # filas de la matriz = [nombre de usuario,[ISBN1, ISBN2]]
-historiales = [["meli"], [9780747532743, 9780553573404],
-               ["dani"], [9780007136834, 9780307474728]]
+historiales = [
+    ["meli"],
+    [9780747532743, 9780553573404],
+    ["dani"],
+    [9780007136834, 9780307474728],
+]
